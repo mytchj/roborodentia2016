4d2ef0f39aa2135147ce781f5064b3cf6a2f539b
Darius <dariusmholmgren@gmail.com>
2016-04-09 20:21:14 -0700

    Added files via upload
    My contribution was redo-ing the updateSonar() function.  It is now very precise and properly bundled into the Location.cpp file.

---
+++ Location.h
@@ -0,0 +1,39 @@
+#ifndef Location_h
+#define Location_h
+
+#include <Arduino.h>
+#include "NewPing.h"
+#include "config.h"
+
+typedef struct {int x; int y;} locationCoordinates;
+
+class Location {
+  private:  
+#if DEBUG_ENABLED
+    static int joyXYB[JOYCOUNT];
+#endif
+    static int irRaw[IRCOUNT];
+    static boolean ir[IRCOUNT];
+    static uint8_t cm[SONAR_NUM];
+
+  public:
+    static locationCoordinates myLoc;
+    static void Init();
+    static NewPing sonar[];
+#if DEBUG_ENABLED
+    static int* updateJoystick();
+    static uint8_t joystickDirection(int);
+    
+    static void printJoystickPosition();
+    static void printSonar();
+    static void printInfrared();
+    static void printRawInfrared();
+#endif
+    static boolean* updateInfrared();
+    static bool irMap(int);
+    static uint8_t* updateSonarQuick();
+    static uint8_t* updateSonar();
+    static void echoCheck();
+};
+
+#endif
